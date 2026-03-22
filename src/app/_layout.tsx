885d63fef5797e4b61ffbcd8769f1e8d2453101a
JJ <153640426+KodeckJames@users.noreply.github.com>
2026-03-22 14:39:38 +0300

    Chore: Set up required Providers

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,32 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
-import '@/global.css';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native'
+import React from 'react'
+import { useColorScheme } from 'react-native'
+import '@/global.css'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { KeyboardProvider } from 'react-native-keyboard-controller'
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { AnimatedSplashOverlay } from '@/components/animated-icon'
+import AppTabs from '@/components/app-tabs'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+    <KeyboardProvider>
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <ThemeProvider
+            value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+          >
+            <AnimatedSplashOverlay />
+            <AppTabs />
+          </ThemeProvider>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </KeyboardProvider>
+  )
 }
