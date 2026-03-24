10dcd0d7473bd7d24a7850ce0c07a5d48d473981
JJ <153640426+KodeckJames@users.noreply.github.com>
2026-03-24 10:23:09 +0300

    Fix: Fixed StatusBar rendering

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,14 +5,12 @@ import {
 } from '@react-navigation/native'
 import React from 'react'
 import { useColorScheme } from 'react-native'
-import '@/global.css'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { KeyboardProvider } from 'react-native-keyboard-controller'
-
 import { AnimatedSplashOverlay } from '@/components/animated-icon'
 import AppTabs from '@/components/app-tabs'
-import { StatusBar } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 import { Stack } from 'expo-router'
 import '../../global.css'
 
@@ -21,18 +19,17 @@ export default function TabLayout() {
   return (
     <KeyboardProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView
+          style={{
+            flex: 1,
+            backgroundColor: colorScheme === 'dark' ? '#000' : '#fff',
+          }}
+        >
           <ThemeProvider
             value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
           >
             <AnimatedSplashOverlay />
-            <StatusBar
-              animated
-              backgroundColor={colorScheme === 'dark' ? '#000' : '#fff'}
-              barStyle={
-                colorScheme === 'dark' ? 'light-content' : 'dark-content'
-              }
-            />
+            <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
             <Stack screenOptions={{}}>
               <Stack.Screen
                 name="OnboardingPage"
