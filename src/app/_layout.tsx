a72e7418c8821d51eb683b7d1525ad43eccf3152
JJ <153640426+KodeckJames@users.noreply.github.com>
2026-03-26 01:04:46 +0300

    Wrapped the app in the Convex ConvexProvider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,52 +1,60 @@
+import { AnimatedSplashOverlay } from '@/components/animated-icon'
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native'
+import { ConvexProvider, ConvexReactClient } from 'convex/react'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { useColorScheme } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { KeyboardProvider } from 'react-native-keyboard-controller'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { AnimatedSplashOverlay } from '@/components/animated-icon'
-import AppTabs from '@/components/app-tabs'
-import { StatusBar } from 'expo-status-bar'
-import { Stack } from 'expo-router'
+import { KeyboardProvider } from 'react-native-keyboard-controller'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import '../../global.css'
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+})
 
 export default function TabLayout() {
   const colorScheme = useColorScheme()
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <KeyboardProvider>
-        <SafeAreaProvider>
-          <SafeAreaView
-            style={{
-              flex: 1,
-              backgroundColor: colorScheme === 'dark' ? '#000' : '#fff',
-            }}
-          >
-            <ThemeProvider
-              value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+    <ConvexProvider client={convex}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <KeyboardProvider>
+          <SafeAreaProvider>
+            <SafeAreaView
+              style={{
+                flex: 1,
+                backgroundColor: colorScheme === 'dark' ? '#000' : '#fff',
+              }}
             >
-              <AnimatedSplashOverlay />
-              <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-              <Stack screenOptions={{}}>
-                <Stack.Screen
-                  name="OnboardingPage"
-                  options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                  name="BallotLocation"
-                  options={{ headerTitle: 'HakiVote' }}
-                />
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              </Stack>
-            </ThemeProvider>
-          </SafeAreaView>
-        </SafeAreaProvider>
-      </KeyboardProvider>
-    </GestureHandlerRootView>
+              <ThemeProvider
+                value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+              >
+                <AnimatedSplashOverlay />
+                <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+                <Stack screenOptions={{}}>
+                  <Stack.Screen
+                    name="OnboardingPage"
+                    options={{ headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name="BallotLocation"
+                    options={{ headerTitle: 'HakiVote' }}
+                  />
+                  <Stack.Screen
+                    name="(tabs)"
+                    options={{ headerShown: false }}
+                  />
+                </Stack>
+              </ThemeProvider>
+            </SafeAreaView>
+          </SafeAreaProvider>
+        </KeyboardProvider>
+      </GestureHandlerRootView>
+    </ConvexProvider>
   )
 }
